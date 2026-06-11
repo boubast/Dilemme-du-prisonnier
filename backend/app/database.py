6547f95f36ef0a47e2cc11cf4b0c76a22010a65c
Thomas Carfantan <tcarfantan11@gmail.com>
2026-06-11 15:22:20 +0200

    fix: correction de la récupération des variables d'env dans le back

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,5 +1,8 @@
 import os
 from collections.abc import Generator
+from dotenv import load_dotenv
+
+load_dotenv()
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, Session, sessionmaker
