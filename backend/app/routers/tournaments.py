c11ef41fc99c77765afff0bcc0782bfa080c7a2a
Bendahmane Yanis <yanis.bendahmane@etu.umontpellier.fr>
2026-06-16 14:55:27 +0200

    Ajout du stockage du dernier tournoi en backend

--- backend/app/routers/tournaments.py
+++ backend/app/routers/tournaments.py
@@ -14,6 +14,7 @@ from app.tournoi import Tournoi as TournoiMoteur
 
 router = APIRouter(prefix="/tournament", tags=["tournament"])
 
+lastTournamentStatistics = None
 
 @router.get("", response_model=list[TournoiListRead])
 def list_tournaments(db: Session = Depends(get_db)) -> list[Tournoi]:
@@ -22,30 +23,40 @@ def list_tournaments(db: Session = Depends(get_db)) -> list[Tournoi]:
 
 @router.get("/{tournoi_id}", response_model=TournoiDetailRead)
 def get_tournament(tournoi_id: int, db: Session = Depends(get_db)) -> Tournoi:
-    stmt = (
-        select(Tournoi)
-        .where(Tournoi.id_tournoi == tournoi_id)
-        .options(
-            selectinload(Tournoi.parties).selectinload(Partie.iterations),
-            selectinload(Tournoi.parties).selectinload(Partie.strategie_1),
-            selectinload(Tournoi.parties).selectinload(Partie.strategie_2),
-            selectinload(Tournoi.participations).selectinload(Participation.strategie),
-        )
-    )
-    tournoi = db.scalars(stmt).first()
-    tournoiStats = TournoiMoteur(tournoi.id_tournoi)
-    tournoiStats.generer_statistiques()
 
-    if tournoi is None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Tournoi introuvable",
+    global lastTournamentStatistics
+
+    if lastTournamentStatistics is None or lastTournamentStatistics.id_tournoi != tournoi_id :
+
+        stmt = (
+            select(Tournoi)
+            .where(Tournoi.id_tournoi == tournoi_id)
+            .options(
+                selectinload(Tournoi.parties).selectinload(Partie.iterations),
+                selectinload(Tournoi.parties).selectinload(Partie.strategie_1),
+                selectinload(Tournoi.parties).selectinload(Partie.strategie_2),
+                selectinload(Tournoi.participations).selectinload(Participation.strategie),
+            )
         )
-    return tournoiStats
+        tournoi = db.scalars(stmt).first()
+        tournoiStats = TournoiMoteur(tournoi.id_tournoi)
+        tournoiStats.generer_statistiques()
+
+        if tournoi is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Tournoi introuvable",
+            )
+        return tournoiStats
+
+    else :
+        return lastTournamentStatistics
 
 
 @router.post("/launch", status_code=status.HTTP_201_CREATED)
 def launch_tournament(payload: TournamentLaunchCreate, db: Session = Depends(get_db)) -> dict[str, int | str]:
+
+    global lastTournamentStatistics
 
     tournoi = TournoiMoteur.create_tournoi(payload.nb_iterations,
                             payload.cout_coop_coop,
@@ -54,6 +65,9 @@ def launch_tournament(payload: TournamentLaunchCreate, db: Session = Depends(get
                             payload.cout_trahi_trahi,
                             payload.strategie_ids)
     tournoi.execute()
+
+    tournoi.generer_statistiques()
+    lastTournamentStatistics = tournoi
 
     return {
         "id_tournoi": tournoi.id_tournoi,
