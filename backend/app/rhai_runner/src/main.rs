f30ba1d718dc1ddf192b03b258cf1d83be099844
Julien SIMON <julien.simon@etu.univ-nantes.fr>
2026-06-16 11:48:11 +0200

    Passage en Enum pour les return + ajout stratégies

--- backend/app/rhai_runner/src/main.rs
+++ backend/app/rhai_runner/src/main.rs
@@ -1,7 +1,15 @@
-use rhai::{Engine, EvalAltResult};
+use rhai::{Engine, EvalAltResult, Dynamic};
 use rhai::packages::Package;
 use rhai_rand::RandomPackage;
 use std::env;
+use rhai::plugin::*;
+
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+enum Choice {
+    Cooperate,
+    Betray,
+}
 
 fn main() -> Result<(), Box<EvalAltResult>> {
 
@@ -20,6 +28,10 @@ fn main() -> Result<(), Box<EvalAltResult>> {
 
     let mut engine = Engine::new();
 
+    // Ajout du module de gestion des Enums
+    engine.register_type_with_name::<Choice>("Choice")
+        .register_static_module("Choice", exported_module!(choice_enum_module).into());
+
     // Ajout de la gestion d'aléatoire
     engine.register_global_module(RandomPackage::new().as_shared_module());
 
@@ -31,8 +43,11 @@ fn main() -> Result<(), Box<EvalAltResult>> {
                         let cout_trahison = {cout_trahison};
                         let cout_cooperation = {cout_cooperation};
                         let cout_trahison_cooperation = {cout_trahison_cooperation};
-                        let cout_cooperation_trahison = {cout_cooperation_trahison};");
-    let renvoi_choix = ";};choix();";
+                        let cout_cooperation_trahison = {cout_cooperation_trahison};
+                        ");
+    let renvoi_choix = "
+                        ;};
+                        choix().value;";
     let script_complet = format!("{debut_fonction}{init_variables}{script}{renvoi_choix}");
 
     // Exécution du script
@@ -48,10 +63,56 @@ fn main() -> Result<(), Box<EvalAltResult>> {
     }
 }
 
-// depuis Windows
-// cargo build --release
-// inutilisé : cargo build --release --target wasm32-wasip1
+// cargo build --release --target wasm32-wasip1
 
+// Create a plugin module with functions constructing the 'Choice' variants
+#[export_module]
+mod choice_enum_module {
+    // Constructors for 'Choice' variants
+    pub const COOPERATE: Choice = Choice::Cooperate;
 
-// depuis Linux (WSL)
-// cargo build --target x86_64-unknown-linux-gnu --release
+    pub const BETRAY: Choice = Choice::Betray;
+
+    /// Return the inner value.
+    #[rhai_fn(global, get = "value", pure)]
+    pub fn get_value(my_enum: &mut Choice) -> String {
+        match my_enum {
+            Choice::Cooperate => "0".to_string(),
+            Choice::Betray => "1".to_string(),
+        }
+    }
+
+    // Access to inner values by position
+
+    /// Return the value kept in the first position of `Choice`.
+    #[rhai_fn(global, get = "field_0", pure)]
+    pub fn get_field_0(my_enum: &mut Choice) -> Dynamic {
+        match my_enum {
+            Choice::Cooperate => Dynamic::UNIT,
+            Choice::Betray => Dynamic::UNIT,
+        }
+    }
+    /// Return the value kept in the second position of `Choice`.
+    #[rhai_fn(global, get = "field_1", pure)]
+    pub fn get_field_1(my_enum: &mut Choice) -> Dynamic {
+        match my_enum {
+            Choice::Cooperate | Choice::Betray => Dynamic::UNIT,
+        }
+    }
+
+    // Printing
+    #[rhai_fn(global, name = "to_string", name = "to_debug", pure)]
+    pub fn to_string(my_enum: &mut Choice) -> String {
+        format!("{my_enum:?}")
+    }
+
+    // '==' and '!=' operators
+    #[rhai_fn(global, name = "==", pure)]
+    pub fn eq(my_enum: &mut Choice, my_enum2: Choice) -> bool {
+        my_enum == &my_enum2
+    }
+    #[rhai_fn(global, name = "!=", pure)]
+    pub fn neq(my_enum: &mut Choice, my_enum2: Choice) -> bool {
+        my_enum != &my_enum2
+    }
+}
